11c6685c8541d1b0e89d361c1ad085ceb8f9ca59
Роман Назмиев <roma.nazmiev@gmail.com>
2023-08-30 12:04:27 +0700

    skeleton edit

--- src/components/JokeBlock/skeleton.tsx
+++ src/components/JokeBlock/skeleton.tsx
@@ -3,13 +3,13 @@ import ContentLoader from "react-content-loader"
 const Skeleton = () => (
   <ContentLoader 
     speed={2}
-    width={320}
-    height={320}
-    viewBox="0 0 320 320"
+    width={518}
+    height={212}
+    viewBox="0 0 518 212"
     backgroundColor="#f3f3f3"
     foregroundColor="#ecebeb"
   >
-    <circle cx="160" cy="160" r="160" />
+    <rect x="0" y="0" rx="9" ry="9" width="518" height="212" />
   </ContentLoader>
 )
 
